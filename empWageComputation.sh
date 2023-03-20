e990a7cfbbbd6bfbe9281b6865bb35f3834d20e9
kirtimadam <104613258+kirtimadam@users.noreply.github.com>
2023-03-21 00:03:58 +0530

    add third program

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -12,3 +12,19 @@ else
 echo "Empoloyee is not present";
 
 fi
+ispresentt=1;
+randomcheckk="$((RANDOM%2))";
+
+if [ $ispresentt -eq $randomcheckk ];
+then
+       empratehr=20;
+        emphr=8;
+salary=$(($empratehr*$emphr));
+
+echo $salary;
+
+else
+
+salary=0;
+
+fi
